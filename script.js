e13d7d6c476b07146278bdb66c24d66c22bd4b82
hanag1234 <101281787+hanag1234@users.noreply.github.com>
2023-11-06 14:57:02 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// // Identifying our image
-// const smiley = document.querySelector("#smiley");
-
-// // Adding reaction when image is clicked
-// smiley.addEventListener("click", () => {
-//    alert(`Smiley says "Welcome to Hana's page!"`);
-// });
